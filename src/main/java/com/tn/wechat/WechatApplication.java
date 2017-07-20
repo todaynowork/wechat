cb08d648a0d5f83b30fd9cc6ffc5de8ae04bef4e
zhiguo.du <duzhiguo@cn.ibm.com>
2017-07-20 22:47:04 +0800

    war

--- src/main/java/com/tn/wechat/WechatApplication.java
+++ src/main/java/com/tn/wechat/WechatApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WechatApplication {
+public class WechatApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WechatApplication.class, args);
