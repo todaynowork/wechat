a5cd8385a4170041b165147d45b2a4f336ffa13a
JeffMaker <676538399@qq.com>
2017-09-20 17:25:54 +0800

    2017-09-20 17:25

--- src/main/java/com/tn/wechat/dbhelper/DBHelper.java
+++ src/main/java/com/tn/wechat/dbhelper/DBHelper.java
@@ -13,11 +13,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  */
 public class DBHelper{
 	public SqlSession getSqlSession() throws IOException{
-		//通过配置文件获取数据库连接
 		Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-		//获取sqlsessionfactory
 		SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(reader);
-		//通过sqlsessionfactory打开一个session
 		SqlSession sqlSession = ssf.openSession();
 		return sqlSession;
 	}
