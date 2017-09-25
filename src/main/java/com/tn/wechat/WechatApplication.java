e3c1ab2e3ebff69acfffd959816a3be78aef797e
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-09-25 09:21:24 +0800

    add log

--- src/main/java/com/tn/wechat/WechatApplication.java
+++ src/main/java/com/tn/wechat/WechatApplication.java
@@ -1,5 +1,7 @@
 package com.tn.wechat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,14 +10,15 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class WechatApplication extends SpringBootServletInitializer
 {
-    
+    private static Logger logger = LoggerFactory.getLogger(WechatApplication.class);
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        logger.info(" start from servlet 3.0 initializer");
         return application.sources(WechatApplication.class);
     }
 
 	public static void main(String[] args) {
-        //System.out.print("hello");
+        logger.info(" start from command line");
 		SpringApplication.run(WechatApplication.class, args);
 	}
 }
