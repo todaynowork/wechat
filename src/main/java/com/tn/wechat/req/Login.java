0c6755340d55b5f8792a6d62991bce3549c29ff5
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-10-01 20:43:52 +0800

    move code to parameter

---
+++ src/main/java/com/tn/wechat/req/Login.java
@@ -0,0 +1,13 @@
+package com.tn.wechat.req;
+
+public class Login {
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
