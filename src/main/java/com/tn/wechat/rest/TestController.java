903cf7356e212ce856d4addacbe7ecc3709126a0
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-11-20 10:17:44 +0800

    add mybatis entity & mapper

--- src/main/java/com/tn/wechat/rest/TestController.java
+++ src/main/java/com/tn/wechat/rest/TestController.java
@@ -1,9 +1,13 @@
 package com.tn.wechat.rest;
 
-        import com.mybatis.cli.AnswerMapper;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import com.mybatis.Question;
+import com.mybatis.User;
+import com.mybatis.cli.AnswerMapper;
+import com.mybatis.cli.QuestionMapper;
+import com.mybatis.cli.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
@@ -11,8 +15,30 @@ public class TestController {
     @Autowired
     AnswerMapper answer;
 
+    @Autowired
+    QuestionMapper questionsMapper;
+
+    @Autowired
+    UserMapper usersMapper;
+
+
     @RequestMapping(path = "/json")
     String helloJson(){
+
+        User user = new User();
+        user.setOpenId("123456");
+        user.setEmail("ddddd");
+        user.setNickName("dddddddccc");
+        user.setAvatorUrl("dddddddccc");
+
+        usersMapper.insert(user);
+
+        Question ques= new Question();
+
+        ques.setDesc("ddddesc");
+        ques.setOwner("123456");
+
+        questionsMapper.insert(ques);
         answer.selectByPrimaryKey( new Integer(1));
         return "{\"msg\":\"hello\"}";
     }
