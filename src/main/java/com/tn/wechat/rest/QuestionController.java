8c93c796263bf3cd0a948cf9c0f84ddfac944937
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-11-26 16:39:24 +0800

    add favorite to question

--- src/main/java/com/tn/wechat/rest/QuestionController.java
+++ src/main/java/com/tn/wechat/rest/QuestionController.java
@@ -1,6 +1,9 @@
 package com.tn.wechat.rest;
 
+import com.mybatis.FavoriteAnswer;
+import com.mybatis.FavoriteAnswerExample;
 import com.mybatis.Question;
+import com.mybatis.cli.FavoriteAnswerMapper;
 import com.mybatis.cli.QuestionMapper;
 import com.tn.wechat.util.IMyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,15 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
+@RequestMapping("/question")
 public class QuestionController {
+
+    private FavoriteAnswerMapper favoriteAnswerMapper;
+
+    @Autowired
+    public void setFavoriteAnswerMapper(FavoriteAnswerMapper favoriteAnswerMapper){
+        this.favoriteAnswerMapper = favoriteAnswerMapper;
+    }
 
     private QuestionMapper questionMapper;
 
@@ -22,26 +33,26 @@ public class QuestionController {
         this.utils = utils;
     }
 
-    @GetMapping("/question/my/{userId}")
+    @GetMapping("/my/{userId}")
     public @ResponseBody
     List<Question> myQuestions(@PathVariable Integer userId, HttpSession session) {
         return questionMapper.selectMyQuestions(userId);
     }
 
-    @GetMapping("/question/all")
+    @GetMapping("/all")
     public @ResponseBody
     List<Question> allQuestions() {
         System.out.println("test dev tool 111");
         return questionMapper.selectAllQuestions();
     }
 
-    @GetMapping("/question/{quesId}/detail")
+    @GetMapping("/{quesId}/detail")
     public @ResponseBody
     Question allQuestionsWithAnswer(@PathVariable Integer quesId) {
         return questionMapper.selectByPrimaryKeyWithAnswer(quesId);
     }
 
-    @PostMapping("/question")
+    @PostMapping("")
     public @ResponseBody
     Question createQuestion(@RequestBody Question question) {
         question.setUpdateTime(utils.getCurrentTimeStamp());
@@ -54,10 +65,26 @@ public class QuestionController {
         return question;
     }
 
-    @DeleteMapping("/question/{quesId}")
+    @DeleteMapping("/{quesId}")
     public @ResponseBody
     void deleteQuestion(@PathVariable Integer quesId, HttpSession session) {
         this.questionMapper.deleteByPrimaryKey(quesId);
+    }
+
+    @PostMapping("/favorite")
+    public FavoriteAnswer markFavorite(@RequestBody FavoriteAnswer favoriteAnswer){
+
+        FavoriteAnswerExample example = new FavoriteAnswerExample();
+        example.createCriteria().andQuestionIdEqualTo(favoriteAnswer.getQuestionId());
+        long lst =  favoriteAnswerMapper.countByExample(example);
+//        favoriteAnswer.setIsFavorite("Y");
+        if(lst>0){
+            favoriteAnswerMapper.updateByPrimaryKey(favoriteAnswer);
+        }else{
+            favoriteAnswerMapper.insert(favoriteAnswer);
+        }
+
+        return favoriteAnswer;
     }
 
     @Autowired
